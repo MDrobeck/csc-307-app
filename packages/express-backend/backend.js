2d550f36465c449b88f42dd193358f27fa21a707
Minksthesinks2 <mjdrobeck@gmail.com>
2025-01-20 19:19:10 -0800

    POST

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,43 +5,59 @@ const app = express();
 const port = 8000;
 
 const users = {
-    users_list: [
+    "users_list": [ //used Boomerang to POST to http://localhost:8000/users Cindy, then used get to GET this list from there, and pasted it into here to make it permanent
       {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
+        "id": "xyz789",
+        "name": "Charlie",
+        "job": "Janitor"
       },
       {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
+        "id": "abc123",
+        "name": "Mac",
+        "job": "Bouncer"
       },
       {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
+        "id": "ppp222",
+        "name": "Mac",
+        "job": "Professor"
       },
       {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
+        "id": "yat999",
+        "name": "Dee",
+        "job": "Aspring actress"
       },
       {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
+        "id": "zap555",
+        "name": "Dennis",
+        "job": "Bartender"
+      },
+      {
+        "id": "qwe123",
+        "job": "Zookeeper",
+        "name": "Cindy"
       }
     ]
-  };
+  }
 
-app.use(express.json());
+app.use(express.json());  
 
-const findUserByName = (name) => {
+  const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
   };
   
+  app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
+
   const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
   
@@ -53,6 +69,17 @@ const findUserByName = (name) => {
     } else {
       res.send(result);
     }
+  });
+
+  const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
   });
 
 app.listen(port, () => {
