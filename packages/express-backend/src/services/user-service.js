18bf1b581c1c56768dd38a358b0ed818087fe65f
Michael Drobeck <135671183+MDrobeck@users.noreply.github.com>
2025-02-04 21:45:44 -0800

    Update user-service.js

--- packages/express-backend/src/services/user-service.js
+++ packages/express-backend/src/services/user-service.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
 import userModel from "../models/user.js";
 
-mongoose.set("debug", true);
+// mongoose.set("debug", true);
 
-mongoose
-  .connect("mongodb://localhost:27017/users", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .catch((error) => console.log(error));
+// mongoose
+//   .connect("mongodb://localhost:27017/users", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .catch((error) => console.log(error));
 
-  
 function getUsers(name, job) {
   let promise;
   if (name === undefined && job === undefined) {
@@ -19,7 +18,10 @@ function getUsers(name, job) {
     promise = findUserByName(name);
   } else if (job && !name) {
     promise = findUserByJob(job);
+  } else if (job && name){
+    promise = findUserByNameandJob(name, job);
   }
+
   return promise;
 }
 
@@ -41,10 +43,20 @@ function findUserByJob(job) {
   return userModel.find({ job: job });
 }
 
+function findUserByNameandJob (name, job){
+  return userModel.find({name:name, job:job});
+}
+
+function deleteUserById(id){
+  return userModel.findByIdAndDelete(id);
+}
+
 export default {
   addUser,
   getUsers,
   findUserById,
   findUserByName,
   findUserByJob,
+  findUserByNameandJob,
+  deleteUserById
 };
